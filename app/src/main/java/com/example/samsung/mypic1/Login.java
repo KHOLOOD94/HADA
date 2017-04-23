e22a2c8e3f11845f70f843625d086a5905d8ac30
KHOLOOD <kholodal94@outlook.sa>
2017-04-23 19:19:58 +0300

    change

--- app/src/main/java/com/example/samsung/mypic1/Login.java
+++ app/src/main/java/com/example/samsung/mypic1/Login.java
@@ -2,12 +2,25 @@ package com.example.samsung.mypic1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
+    EditText UsernameEt, PasswordEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        UsernameEt = (EditText)findViewById(R.id.Username);
+        PasswordEt = (EditText)findViewById(R.id.password);
     }
+
+    public void login(View view){
+    String username = UsernameEt.getText().toString();
+    String password = PasswordEt.getText().toString();
+    String method = "login";
+    BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(method,username,password);
+}
 }
