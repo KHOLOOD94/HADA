85c6279e25951ea4697222178aad61e505ff037f
KHOLOOD <kholodal94@outlook.sa>
2017-04-23 09:02:42 +0300

    change

---
+++ app/src/main/java/com/example/samsung/mypic1/BackgroundTask.java
@@ -0,0 +1,106 @@
+package com.example.samsung.mypic1;
+
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class BackgroundTask extends AsyncTask<String ,Void , String> {
+
+    Context cont;
+    BackgroundTask(Context c) {
+
+        this.cont = c;
+    }
+
+    @Override
+    protected String doInBackground(String... voids) {
+
+        String reg_url = "http://kholood.heliohost.org/Register.php";
+
+        String method = voids[0];
+        URL url;
+        HttpURLConnection httpURLConnection;
+        OutputStream os;
+        BufferedWriter bufferedWriter;
+        String data;
+        InputStream is;
+
+        if(method.equalsIgnoreCase("register".trim())){
+
+            String name = voids[1];
+            String email = voids[2];
+            String contact = voids[3];
+            String password = voids[4];
+
+
+
+            try {
+                url = new URL(reg_url);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                os = httpURLConnection.getOutputStream();
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+
+
+                data = URLEncoder.encode("name","UTF-8")+"="+URLEncoder.encode(name,"UTF-8")+"&"+
+                        URLEncoder.encode("email","UTF-8")+"="+URLEncoder.encode(email,"UTF-8")+"&"+
+                        URLEncoder.encode("contact","UTF-8")+"="+URLEncoder.encode(contact,"UTF-8")+"&"+
+                        URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                os.close();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line;
+                while((line = bufferedReader.readLine())!= null) {
+                    result += line;
+                    break;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(cont,result,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+}
+
--- app/src/main/java/com/example/samsung/mypic1/HomePage.java
+++ app/src/main/java/com/example/samsung/mypic1/HomePage.java
@@ -1,12 +1,21 @@
 package com.example.samsung.mypic1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+    }
+
+    public void createAccount(View view){
+        Intent intent = new Intent(this,Register.class);
+    }
+    public void login(View view){
+        Intent intent = new Intent(this,Login.class);
     }
 }
