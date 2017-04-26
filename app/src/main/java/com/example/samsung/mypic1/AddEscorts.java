2d3913d77266e2e9c523f308376accc272afb91c
KHOLOOD <kholodal94@outlook.sa>
2017-04-26 16:32:06 +0300

    change

--- app/src/main/java/com/example/samsung/mypic1/AddEscorts.java
+++ app/src/main/java/com/example/samsung/mypic1/AddEscorts.java
@@ -2,6 +2,7 @@ package com.example.samsung.mypic1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddEscorts extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class AddEscorts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addescorts);
+    }
+
+    public void addEscorts(View view){
+
     }
 }
