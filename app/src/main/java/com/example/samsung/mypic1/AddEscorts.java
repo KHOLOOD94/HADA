cfa6713fe42bc219723b7a4f72206fd82174e766
KHOLOOD <kholodal94@outlook.sa>
2017-04-26 16:47:20 +0300

    change

--- app/src/main/java/com/example/samsung/mypic1/AddEscorts.java
+++ app/src/main/java/com/example/samsung/mypic1/AddEscorts.java
@@ -3,16 +3,26 @@ package com.example.samsung.mypic1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddEscorts extends AppCompatActivity {
 
+    EditText esc_name , esc_phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addescorts);
+
+        esc_name = (EditText)findViewById(R.id.editTextName);
+        esc_phone = (EditText) findViewById(R.id.editTextPhone);
     }
 
     public void addEscorts(View view){
+
+        String name = esc_name.getText().toString();
+        String phone = esc_phone.getText().toString();
+
+
 
     }
 }
