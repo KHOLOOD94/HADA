0b2038449f54fb056b12871bf974f7340c61ce88
KHOLOOD <kholodal94@outlook.sa>
2017-05-16 02:40:21 +0300

    change

--- app/src/main/java/com/example/samsung/mypic1/Alert.java
+++ app/src/main/java/com/example/samsung/mypic1/Alert.java
@@ -2,7 +2,9 @@ package com.example.samsung.mypic1;
 
 import android.Manifest;
 import android.app.ProgressDialog;
+import android.app.Service;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -10,14 +12,17 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.os.IBinder;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -28,9 +33,14 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Alert extends AppCompatActivity {
-    private TextView t;
-    private LocationManager locationManager;
-    private LocationListener listener;
+
+    TextView t;
+    private static final int REQUEST_CODE_PERMISSION = 2;
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+    // GPSTracker class
+    GPSTracker gps;
+
+
     String json_string;
     String JSON_STRING;
 
@@ -38,73 +48,54 @@ public class Alert extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alert);
-       // getRelativeNumber();
+
+        try {
+            if (ActivityCompat.checkSelfPermission(this, mPermission)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{mPermission},
+                        REQUEST_CODE_PERMISSION);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        loc();
+    }
+
+    public void loc() {
         t = (TextView) findViewById(R.id.LocationText);
+        gps = new GPSTracker(Alert.this);
 
+        // check if GPS enabled
+        if (gps.canGetLocation()) {
 
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
 
-
-        listener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-
-                    t.append(location.getLongitude() + "\n," + location.getLatitude());
-
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-
-                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(i);
-            }
-        };
-
-        configure_button();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case 10:
-                configure_button();
-                break;
-            default:
-                break;
-        }
-    }
-
-    void configure_button() {
-        // first check for permissions
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
-                        , 10);
-
-            }
-            return;
+            // \n is for new line
+            Toast.makeText(getApplicationContext(), "Your Location is - \nLat: "
+                    + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+            t.setText("Your Location is - \nLat: "
+                    + latitude + "\nLong: " + longitude);
         } else {
-            locationManager.requestLocationUpdates("gps", 1000, 1, listener);
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
         }
+
     }
-   public void getRelativeNumber() {
+}
+
+
+   /*public void getRelativeNumber() {
 
         new BackgroundTask().execute();
 
     }
 
-/*
+
     class BackgroundTask extends AsyncTask<Void,Void,String> {
 
         String url_string="http://kholood.heliohost.org/getPhoneNumber.php";
@@ -166,4 +157,4 @@ public class Alert extends AppCompatActivity {
             super.onProgressUpdate(values);
         }
     }*/
-}
+
