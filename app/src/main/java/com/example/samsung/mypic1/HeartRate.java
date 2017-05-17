ab1dccff29f61cdc9fa79e6feebd953c4f899380
KHOLOOD <kholodal94@outlook.sa>
2017-05-17 15:44:45 +0300

    change

--- app/src/main/java/com/example/samsung/mypic1/HeartRate.java
+++ app/src/main/java/com/example/samsung/mypic1/HeartRate.java
@@ -2,25 +2,25 @@ package com.example.samsung.mypic1;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class HeartRate extends AppCompatActivity {
 
     int abnormalRate = 1000;
-    EditText heartRate;
+    TextView heartRate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_rate);
 
-        heartRate = (EditText) findViewById(R.id.heartRate);
+        heartRate = (TextView) findViewById(R.id.heartRate);
 
+        heartRate.setText("10000");
         int rate = Integer.parseInt(heartRate.getText().toString().trim());
         if( rate >= abnormalRate){
             sendSMSMessage();
@@ -29,15 +29,10 @@ public class HeartRate extends AppCompatActivity {
     }
     protected void sendSMSMessage() {
 
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.SEND_SMS)
-                != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.SEND_SMS)) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
             } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.SEND_SMS},
-                        0);
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 0);
             }
         }
     }
@@ -46,12 +41,10 @@ public class HeartRate extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
         switch (requestCode) {
             case 0: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage("0545129728", null, "Shit happens", null, null);
-                    Toast.makeText(getApplicationContext(), "SMS sent.",
-                            Toast.LENGTH_LONG).show();
+                    smsManager.sendTextMessage("+966545129728", null, "Shit happens", null, null);
+                    Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(getApplicationContext(),
                             "SMS faild, please try again.", Toast.LENGTH_LONG).show();
