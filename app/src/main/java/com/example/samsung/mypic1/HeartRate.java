a4685e3671e6a178ad0badefcc8f7efce7aff8c0
KHOLOOD <kholodal94@outlook.sa>
2017-05-17 15:27:47 +0300

    change

--- app/src/main/java/com/example/samsung/mypic1/HeartRate.java
+++ app/src/main/java/com/example/samsung/mypic1/HeartRate.java
@@ -1,13 +1,64 @@
 package com.example.samsung.mypic1;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class HeartRate extends AppCompatActivity {
 
+    int abnormalRate = 1000;
+    EditText heartRate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_rate);
+
+        heartRate = (EditText) findViewById(R.id.heartRate);
+
+        int rate = Integer.parseInt(heartRate.getText().toString().trim());
+        if( rate >= abnormalRate){
+            sendSMSMessage();
+        }
+
+    }
+    protected void sendSMSMessage() {
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        0);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 0: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage("0545129728", null, "Shit happens", null, null);
+                    Toast.makeText(getApplicationContext(), "SMS sent.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }
+
     }
 }
